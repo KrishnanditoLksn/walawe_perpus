10779d3980ab17384c7d4d71e4a64e889148dc91
Gorthdzx <krislaksana123@gmail.com>
2024-05-18 22:57:17 +0700

    feat : membuat beberapa tabel  untuk perpustakaan tahap #1

---
+++ database/migrations/2024_05_18_053507_create_copy_buku_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('copy_buku', function (Blueprint $table) {
+            $table->id('id_copy')->primary();
+            $table->bigInteger('id_buku')->unsigned();
+            $table->integer('no_copy_buku')->unsigned();
+            $table->foreign('id_buku')->references('id_buku')->on('buku');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('copy_buku');
+    }
+};
