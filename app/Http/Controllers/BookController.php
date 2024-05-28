10047e0e0e3bca7f694cd9a3dadcb13a2e01869a
Gorthdzx <krislaksana123@gmail.com>
2024-05-28 08:32:13 +0700

    feat : menambahkan  controller buku

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BookController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'judul' => 'required|max:255',
+            'nama_penulis' => 'required|max:255',
+            'isbn' => 'required|max:255',
+            'penerbit' => 'required|max:255',
+            'tahun_terbit' => 'required'
+        ]);
+        $datas = $request->all();
+        $book = new Book;
+        $book->judul = $datas['judul'];
 
+        return redirect('');
     }
 
     public function update(Request $request, $id)
     {
 
+    }
+
+    public function delete(Request $request, $id)
+    {
+
+    }
+
+    public function show(): View
+    {
+        return view('');
     }
 }
