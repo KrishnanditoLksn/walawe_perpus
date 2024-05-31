827b48e6fd137cd44d1733454af192974963a28c
Gorthdzx <krislaksana123@gmail.com>
2024-05-31 22:31:15 +0700

    feat : menambah kontroller perpustakaan hasil gabungan kategori dan show buku

---
+++ app/Http/Controllers/PerpustakaanController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
+
+class PerpustakaanController extends Controller
+{
+    public function show(): View
+    {
+        $posts = Kategori::all();
+        $books = DB::table('kategori')
+            ->join('buku', 'buku.id_kategori_buku', '=', 'kategori.id')
+            ->select('kategori.id', 'kategori.nama_kategori', 'buku.judul')
+            ->get();
+        $title = 'Laporan';
+        return view('Home', compact('posts', 'books', 'title'));
+    }
+}
