e3730fde698c4e92e2db87a54b827e957637c22c
Gorthdzx <krislaksana123@gmail.com>
2024-06-01 22:38:29 +0700

    fix : menampilkan id setiap judul buku

--- app/Http/Controllers/PerpustakaanController.php
+++ app/Http/Controllers/PerpustakaanController.php
@@ -13,7 +13,7 @@ class PerpustakaanController extends Controller
         $posts = Kategori::all();
         $books = DB::table('kategori')
             ->join('buku', 'buku.id_kategori_buku', '=', 'kategori.id')
-            ->select('kategori.id', 'kategori.nama_kategori', 'buku.judul')
+            ->select('kategori.id', 'kategori.nama_kategori', 'buku.judul' , 'buku.id')
             ->get();
         $title = 'Laporan';
         return view('Home', compact('posts', 'books', 'title'));
