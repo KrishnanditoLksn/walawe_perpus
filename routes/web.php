16dfcbe1abaeb6d05a1ddbdaa1a7cb7c1fd9b81b
RamaSantaa <ramasp839@gmail.com>
2024-06-04 20:03:27 +0700

    menambahkan route ger dan route post login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,16 @@ Route::get('/peminjaman', function () {
 Route::post('/peminjaman', function () {
     return view('peminjaman', ['title' => 'Peminjaman']);
 });
+Route::get('/login', function () {
+    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
+});
+Route::post('/login', function () {
+    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
+});
+
 
 Route::get('/daftarbuku', [CategoriController::class, 'show']);
 Route::post('/daftarbuku', [CategoriController::class, 'show']);
+Route::get('/login', [CategoriController::class, 'show']);
+Route::post('/login', [CategoriController::class, 'show']);
+
