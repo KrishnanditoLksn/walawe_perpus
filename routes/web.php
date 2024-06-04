54eb021ce21257c53ad93f294d6d4a8ce34beb0a
Gorthdzx <krislaksana123@gmail.com>
2024-06-04 13:57:49 +0700

    feat : rute

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\CategoriController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PerpustakaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,11 +10,12 @@ Route::get('/', function () {
 
 
 Route::get('/peminjaman', function () {
-    return view('peminjaman', ['title' => 'Peminjaman']);
+    return view('peminjaman', ['title' => 'PeminjamanKontroller']);
 });
 Route::post('/peminjaman', function () {
-    return view('peminjaman', ['title' => 'Peminjaman']);
+    return view('peminjaman', ['title' => 'PeminjamanKontroller']);
 });
 
-Route::get('/daftarbuku', [CategoriController::class, 'show']);
-Route::post('/daftarbuku', [CategoriController::class, 'show']);
+
+Route::post("tambah-buku",[BookController::class,'store']);
+Route::get('/daftarbuku',[PerpustakaanController::class,'show']);
