e934c0d5bc854ab14894afb6b4df3aa3ec7a349c
Gorthdzx <krislaksana123@gmail.com>
2024-06-05 10:58:26 +0700

    fix : routing di halaman

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\PerpustakaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/daftarbuku');
+    return redirect('/login');
 });
-
-Route::get('/daftarbuku', [PerpustakaanController::class, 'show']);
-
 Route::get('/peminjaman', function () {
     return view('peminjaman', ['title' => 'Peminjaman']);
 });
+Route::post('/peminjaman', function () {
+    return view('peminjaman', ['title' => 'Peminjaman']);
+})->name('log');
+Route::get('/login', function () {
+    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
+});
+Route::post('/login', function () {
+    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
+});
+Route::get('/daftarbuku', [PerpustakaanController::class, 'show']);
+Route::post('/daftarbuku', [PerpustakaanController::class, 'show']);
