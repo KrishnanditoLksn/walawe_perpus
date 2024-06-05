c532523c11a40d09b29d653f7aa12c9d07b1f821
Gorthdzx <krislaksana123@gmail.com>
2024-06-05 10:44:40 +0700

    delete :  error

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,20 @@
 
 use App\Http\Controllers\CategoriController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-    return redirect('/daftarbuku');
+    return redirect('/login');
 });
-
-
 Route::get('/peminjaman', function () {
     return view('peminjaman', ['title' => 'Peminjaman']);
 });
 Route::post('/peminjaman', function () {
-    return view('peminjaman', ['title' => 'Peminjaman']);
-<<<<<<< HEAD
+    return view('peminjaman',   ['title' => 'Peminjaman']);
 })->name('log');
-=======
-});
 Route::get('/login', function () {
     return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
 });
 Route::post('/login', function () {
     return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
 });
-
->>>>>>> 94a118fa0bcc821340be2193dbe18f4a27ac61f5
-
 Route::get('/daftarbuku', [CategoriController::class, 'show']);
 Route::post('/daftarbuku', [CategoriController::class, 'show']);
-//Route::get('/login', [CategoriController::class, 'show']);
-//Route::post('/login', [CategoriController::class, 'show']);
-
