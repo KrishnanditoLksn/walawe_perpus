67953eeef6b7d34292567ba2776a0f357c428e2b
Gorthdzx <krislaksana123@gmail.com>
2024-06-05 08:47:12 +0700

    fix : bug di Kategori kontroller

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,13 @@
 <?php
 
-use App\Http\Controllers\CategoriController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/daftarbuku');
+    return view('Home' , ['title'=>'Home']);
 });
-
-
 Route::get('/peminjaman', function () {
     return view('peminjaman', ['title' => 'Peminjaman']);
 });
-Route::post('/peminjaman', function () {
-    return view('peminjaman', ['title' => 'Peminjaman']);
-<<<<<<< HEAD
-})->name('log');
-=======
+Route::get('/daftarbuku' , function(){
+    return view('Home' , ['title'=>'Daftar buku']);
 });
-Route::get('/login', function () {
-    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
-});
-Route::post('/login', function () {
-    return view('login', ['title' => 'login ']);// login adalah nama file blade untuk halaman login, sesuaikan dengan nama file Anda
-});
-
->>>>>>> 94a118fa0bcc821340be2193dbe18f4a27ac61f5
-
-Route::get('/daftarbuku', [CategoriController::class, 'show']);
-Route::post('/daftarbuku', [CategoriController::class, 'show']);
-//Route::get('/login', [CategoriController::class, 'show']);
-//Route::post('/login', [CategoriController::class, 'show']);
-
