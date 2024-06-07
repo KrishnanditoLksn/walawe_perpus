ef4ccd941dfcbabe3c44eeb7b75adee4444b0cab
Gorthdzx <krislaksana123@gmail.com>
2024-06-07 08:38:38 +0700

    feat : menambahkan testing pada web routing

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    /** @test */
+    public function it_checks_the_peminjaman_route(): void
+    {
+        $response = $this->get('/peminjaman');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_checks_the_daftarbuku_route(): void
+    {
+        $response = $this->get('/daftarbuku');
+        $response->assertStatus(200);
+    }
+
+//    public function it_checks_the_login_route(): void
+//    {
+//        $response = $this->get('/login');
+//        $response->assertStatus(404);
+//    }
+}
