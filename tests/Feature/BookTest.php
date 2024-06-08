95ce8dc41b24abc4cfcf239c07013aa46e5093b7
Gorthdzx <krislaksana123@gmail.com>
2024-06-08 14:35:53 +0700

    feat : book test

---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    use RefreshDatabase;
+     /** @test */
+    
+}
