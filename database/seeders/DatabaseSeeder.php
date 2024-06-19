7e8128e16f5776b938dab13baa9dbcc0b7503f61
Gorthdzx <krislaksana123@gmail.com>
2024-06-19 14:26:56 +0700

    feat : seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
             KategoriSeeder::class,
-            BukuSeeder::class
+            BukuSeeder::class,
+            AdminSeeder::class
         ]);
     }
 }
